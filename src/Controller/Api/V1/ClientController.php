76a7add7923ce46ad32e2f7b52838914e47145c6
catsAND <github@left.lv>
2026-05-16 19:16:38 +0300

    fix: code style

--- src/Controller/Api/V1/ClientController.php
+++ src/Controller/Api/V1/ClientController.php
@@ -34,10 +34,10 @@ class ClientController extends AbstractController
         return new JsonResponse($response);
     }
 
-    #[Route('/{client_id}', name: 'api_client_get', methods: ['GET'])]
-    public function get(string $client_id): JsonResponse
+    #[Route('/{clientId}', name: 'api_client_get', methods: ['GET'])]
+    public function get(string $clientId): JsonResponse
     {
-        $client = $this->clientService->getClientById($client_id);
+        $client = $this->clientService->getClientById($clientId);
 
         return new JsonResponse($this->clientTransformer->transform($client));
     }
@@ -50,18 +50,18 @@ class ClientController extends AbstractController
         return new JsonResponse($this->clientTransformer->transform($client), 201);
     }
 
-    #[Route('/{client_id}', name: 'api_client_update', methods: ['PUT'])]
-    public function update(string $client_id, #[MapRequestPayload] ClientDto $clientDto): JsonResponse
+    #[Route('/{clientId}', name: 'api_client_update', methods: ['PUT'])]
+    public function update(string $clientId, #[MapRequestPayload] ClientDto $clientDto): JsonResponse
     {
-        $client = $this->clientService->updateClientById($client_id, $clientDto);
+        $client = $this->clientService->updateClientById($clientId, $clientDto);
 
         return new JsonResponse($this->clientTransformer->transform($client));
     }
 
-    #[Route('/{client_id}', name: 'api_client_delete', methods: ['DELETE'])]
-    public function delete(string $client_id): JsonResponse
+    #[Route('/{clientId}', name: 'api_client_delete', methods: ['DELETE'])]
+    public function delete(string $clientId): JsonResponse
     {
-        $this->clientService->deleteClientById($client_id);
+        $this->clientService->deleteClientById($clientId);
 
         return new JsonResponse();
     }
